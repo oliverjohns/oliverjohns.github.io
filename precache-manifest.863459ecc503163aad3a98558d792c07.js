bd6d6c085c4eda50be6da8d0181352aea58b0a69
oliverjohns <olijo219@student.liu.se>
2021-05-14 12:31:46 +0200

    removed dev files and added build files for github pages

---
+++ precache-manifest.863459ecc503163aad3a98558d792c07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d36517a94f42d6b2e19978297b88c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87eb93e11d0a76df335d",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "7c540f7796307a6aac28",
+    "url": "./static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "87eb93e11d0a76df335d",
+    "url": "./static/js/2.c2774e9e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.c2774e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c540f7796307a6aac28",
+    "url": "./static/js/main.6a9ed19a.chunk.js"
+  },
+  {
+    "revision": "4d3eb6ef4e822854d283",
+    "url": "./static/js/runtime-main.5a133d6e.js"
+  }
+]);
